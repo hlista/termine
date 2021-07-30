3ad9923791f0443b5598658d22abde6dd607ac92
Michael Hlista <hlista91@gmail.com>
2021-07-30 12:39:25 -0500

    Initial project commit. Project generated with command 'mix phx.new --no-webpack --no-ecto --no-html termine'

---
+++ lib/termine/application.ex
@@ -0,0 +1,32 @@
+defmodule Termine.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      # Start the Telemetry supervisor
+      TermineWeb.Telemetry,
+      # Start the PubSub system
+      {Phoenix.PubSub, name: Termine.PubSub},
+      # Start the Endpoint (http/https)
+      TermineWeb.Endpoint
+      # Start a worker by calling: Termine.Worker.start_link(arg)
+      # {Termine.Worker, arg}
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Termine.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+  # Tell Phoenix to update the endpoint configuration
+  # whenever the application is updated.
+  def config_change(changed, _new, removed) do
+    TermineWeb.Endpoint.config_change(changed, removed)
+    :ok
+  end
+end
