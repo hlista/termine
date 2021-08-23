8dea5a89515e90be002612dfcafe8468e4d18243
Michael Hlista <hlista91@gmail.com>
2021-08-23 09:54:54 -0500

    Inventory created on player creation

--- lib/termine/characters.ex
+++ lib/termine/characters.ex
@@ -5,14 +5,21 @@ defmodule Termine.Characters do
 
 	def create_player(%{user_id: user_id, username: username}) do
 		changeset = Player.changeset(%Player{user_id: user_id}, %{username: username})
-		Repo.insert changeset
+		case Repo.insert changeset do
+			{:ok, player} ->
+				create_inventory(player.id)
+				{:ok, player}
+			_ ->
+				{:error, "Cannot create Player"}
+		end
 	end
 
 	def find_player(params) do
 		Actions.find(Player, params)
 	end
 
-	def create_inventory(params) do
-		Actions.create(Inventory, params)
+	defp create_inventory(player_id) do
+		changeset = Inventory.changeset(%Inventory{player_id: player_id}, %{})
+		Repo.insert changeset
 	end
 end
