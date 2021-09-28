01ef28d0327646574e70fadf0d3148d621c9e5b8
Michael Hlista <hlista91@gmail.com>
2021-09-28 11:48:17 -0500

    update_state_type graphql mutation

--- lib/termine/worlds.ex
+++ lib/termine/worlds.ex
@@ -3,6 +3,8 @@ defmodule Termine.Worlds do
 	alias Termine.Worlds.{Node, State, Neighbor}
 	alias EctoShorts.Actions
 
+	@state_types [:state_type_loop_until, :state_type_loop, :state_type_collectable, :state_type_block_until]
+
 	def list_nodes(params) do
 		{:ok, Actions.all(Node, params)}
 	end
@@ -27,7 +29,27 @@ defmodule Termine.Worlds do
 		Actions.update(State, id, params)
 	end
 
+	def update_state_type(id, params) do
+		state = Repo.get(State, id)
+		state = Repo.preload(state, @state_types)
+		params = update_already_existing_state_type(state, params)
+		changeset = Ecto.Changeset.change(state, params)
+		Repo.update(changeset)
+	end
+
 	def create_neighbor(params) do
 		Actions.create(Neighbor, params)
+	end
+
+	defp update_already_existing_state_type(state, params) do
+		Enum.reduce(params, params, fn {state_type, value}, acc ->
+			type = get_in(state, [Access.key(state_type)])
+			if type do
+				Repo.update(Ecto.Changeset.change(type, value))
+				Map.delete(acc, state_type)
+			else
+				acc
+			end
+		end)
 	end
 end
--- lib/termine_web/schema/mutations/state.ex
+++ lib/termine_web/schema/mutations/state.ex
@@ -19,14 +19,20 @@ defmodule TermineWeb.Schema.Mutations.State do
 		field :update_state, :state do
 			arg :id, non_null(:id)
 			arg :inspect_text, :string
-			arg :type, :state_type
 			arg :node_id, :id
+			#middleware TermineWeb.AdminAuthentication
+			resolve &Resolvers.State.update/2
+		end
+
+		field :update_state_type, :state do
+			arg :state_id, non_null(:id)
+			arg :type, :state_type
 			arg :state_type_collectable, :input_state_type_collectable
 			arg :state_type_loop_until, :input_state_type_loop_until
 			arg :state_type_loop, :input_state_type_loop
 			arg :state_type_block_until, :input_state_type_block_until
 			#middleware TermineWeb.AdminAuthentication
-			resolve &Resolvers.State.update/2
+			resolve &Resolvers.State.update_type/2
 		end
 	end
 end
