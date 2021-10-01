beeb392eae398748143695babcce151a24829f85
Michael Hlista <hlista91@gmail.com>
2021-10-01 12:47:38 -0500

    Distributor Genserver now puts the expertise level of a miner inside the cache structure

--- lib/termine/distributor/impl.ex
+++ lib/termine/distributor/impl.ex
@@ -3,7 +3,7 @@ defmodule Termine.Distributor.Impl do
 	alias Termine.Worlds
 	
 	def initialize_state() do
-		{:ok, nodes} = Worlds.list_nodes(%{preload: [miners: [], current_state: [state_type_collectable: [:resource]]]})
+		{:ok, nodes} = Worlds.list_nodes(%{preload: [player_miners: [expertises: []], current_state: [state_type_collectable: []]]})
 		nodes = Enum.reduce(nodes, %{}, fn node, acc -> 
 			if node.state_type === :mineable or node.state_type === :attackable do
 				Map.put(acc, node.id, create_cache_structure(node))
@@ -16,18 +16,22 @@ defmodule Termine.Distributor.Impl do
 
 	def create_cache_structure(node) do
 		%{
-			resource: node.current_state.state_type_collectable.resource,
+			resource_id: node.current_state.state_type_collectable.resource_id,
 			amount_left: node.current_state.state_type_collectable.amount,
-			miners: create_miner_cache_structure(node.miners)
+			miners: create_miner_cache_structure(node.player_miners, node.current_state.state_type_collectable.resource_id)
 		}
 	end
 
-	def create_miner_cache_structure(miners) do
+	def create_miner_cache_structure(miners, resource_id) do
 		miners
 		|> Enum.map(fn miner -> 
-			{miner.id, %{expertise: 1, hits: 0}}
+			{miner.id, %{expertise: get_miner_expertise_level(miner, resource_id), hits: 0}}
 		end)
 		|> Enum.into(%{})
+	end
+
+	def get_miner_expertise_level(miner, resource_id) do
+		Enum.find_value(miner.expertises, fn x -> if x.resource_id === resource_id, do: x.level end)
 	end
 
 	def increment_nodes_miners_hits(nodes) do
