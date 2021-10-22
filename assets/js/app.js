3acfab2c6ba9ecd2e0118615b64402270d7c415a
Michael Hlista <hlista91@gmail.com>
2021-10-22 15:07:05 -0500

    Initial React setup commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,17 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+class HelloReact extends React.Component {
+	render() {
+		return(<h1>Hello React!</h1>)
+	}
+}
+
+ReactDOM.render(
+	<HelloReact/>,
+	document.getElementById("hello-react")
+)
