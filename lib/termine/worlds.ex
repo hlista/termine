baab5a8c750b6b92807d2f10d6340cabe3cf8bb6
Michael Hlista <hlista91@gmail.com>
2021-10-22 10:02:00 -0500

    functionality to unblock nodes on a block_until state

--- lib/termine/worlds.ex
+++ lib/termine/worlds.ex
@@ -44,21 +44,23 @@ defmodule Termine.Worlds do
 	def complete_state(node_id) do
 		node = Repo.get(Node, node_id)
 		update_state(node.current_state_id, %{has_been_completed: true})
+		unblock_nodes(node.current_state_id)
+		increment_state(node)
 	end
 
-	def increment_state(node_id) do
-		node = Repo.get(Node, node_id)
+	def increment_state(node) do
 		current_state_id = node.current_state_id
 		state_id_array = node.state_id_array
 		index = Enum.find_index(state_id_array, fn x -> x === current_state_id end)
 		next_state_id = Enum.fetch!(state_id_array, index + 1)
-		next_state = Repo.get(State, next_state_id)
-		next_state = Repo.preload(next_state, @state_types)
+		next_state = State
+		|> Repo.get(next_state_id)
+		|> Repo.preload([state_type_loop_until: [:until_state], :state_type_loop, :state_type_collectable, state_type_block_until: [:until_state]])
 		case next_state.type do
 			:loop ->
 				Actions.update(Node, node_id, %{current_state_id: next_state.state_type_loop.go_to_state_id})
 			:loop_until ->
-				until_state = Repo.get(State, next_state.state_type_loop_until.until_state_id)
+				until_state = next_state.state_type_loop_until.until_state
 				if (until_state.has_been_completed) do
 					next_state_id = Enum.fetch!(state_id_array, index + 2)
 					Actions.update(Node, node_id, %{current_state_id: next_state_id})
@@ -66,7 +68,7 @@ defmodule Termine.Worlds do
 					Actions.update(Node, node_id, %{current_state_id: next_state.state_type_loop_until.go_to_state_id})
 				end
 			:block_until ->
-				until_state = Repo.get(State, next_state.state_type_block_until.until_state_id)
+				until_state = next_state.state_type_block_until.until_state
 				if (until_state.has_been_completed) do
 					next_state_id = Enum.fetch!(state_id_array, index + 2)
 					Actions.update(Node, node_id, %{current_state_id: next_state_id})
@@ -76,6 +78,14 @@ defmodule Termine.Worlds do
 			_ ->
 				Actions.update(Node, node_id, %{current_state_id: next_state_id})
 		end
+	end
+
+	def unblock_nodes(state_id) do
+		Enum.each(Actions.all(Termine.StateTypes.BlockUntil, %{until_state_id: state_id}), fn block_until_state -> 
+			Enum.each(Actions.all(Node, %{current_state_id: block_until_state.state_id}), fn blocking_node -> 
+				complete_state(blocking_node.id)
+			end)
+		end)
 	end
 
 	defp update_already_existing_state_type(state, params) do
