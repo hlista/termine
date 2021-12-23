c3e551840b6a72d2cd1498353ec967885cfb648c
Michael Hlista <hlista91@gmail.com>
2021-12-23 17:47:28 -0600

    dockerized config file. defaults to local environment if environment variables don't exist

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ use Mix.Config
 
 # Configure your database
 config :termine, Termine.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "termine_dev",
-  hostname: "localhost",
+  username: System.get_env("PGUSER", "postgres"),
+  password: System.get_env("PGPASSWORD", "postgres"),
+  database: System.get_env("PGDATABASE", "termine_dev"),
+  hostname: System.get_env("PGHOST", "localhost"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
