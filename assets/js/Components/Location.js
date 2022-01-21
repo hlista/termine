99f67acff296acf4d9b66c36f701b28d0bad197c
Michael Hlista <hlista91@gmail.com>
2022-01-21 16:13:45 -0600

    Location fields filled with dummy data

--- assets/js/Components/Location.js
+++ assets/js/Components/Location.js
@@ -2,9 +2,17 @@ import React from "react";
 
 function Location(){
   return(
-    <div>
+    <div className="text-center">
       <div className="flex h-24 justify-center">
         <p className="flex text-4xl items-center">DeadRock Mines</p>
+      </div>
+      <div className="flex h-12 justify-center">
+        <p className="flex items-center">Many of the local townsfolk call these mines cursed and the promise of treasure burred deep within a fools quest</p>
+      </div>
+      <div className="flex h-36 justify-center">
+        <p className="flex items-center"> 
+          You find yourself at the entrance of the cave. You light your torch and slowly make your way into mines. The ground is sticky and the air is damp. You can't see much beyond the light of your torch. You pause for a moment to listen to your surroundings. You hear water dripping in the distance, and then you hear a hiss. You turn in the direction of the hiss and find a human sized spider blocking the path.
+        </p>
       </div>
     </div>
   );
